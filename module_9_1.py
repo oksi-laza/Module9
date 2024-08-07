e67addde7666819394ab0e12af5fa5b3719a59ae
oksi-laza <153543859+oksi-laza@users.noreply.github.com>
2024-08-07 16:34:07 +0300

    Add files via upload
    Домашнее задание по теме "Введение в функциональное программирование".
    Задача "Вызов разом".

---
+++ module_9_1.py
@@ -0,0 +1,14 @@
+def apply_all_func(int_list, *functions):
+    """
+    :param int_list (int, float) - список из чисел
+    :param *functions - неограниченное количество функций
+    """
+    results = {}
+    for function in functions:
+        results[function.__name__] = function(int_list)
+    return results
+
+
+# Пример работы кода:
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
